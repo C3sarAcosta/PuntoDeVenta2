665efc387ce642344872bbf8a848f7c798219421
César Acosta <c3sar.adan@gmail.com>
2021-10-19 22:46:28 -0600

    Agregar archivos de proyecto.

---
+++ PuntoDeVenta/Dashboard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVenta
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfiguraciones_Click(object sender, EventArgs e)
+        {
+            lblNombreUsuario.Text = "César Acosta";
+            Form1 fm1 = new Form1();
+            fm1.Show();
+            this.Hide();
+        }
+    }
+}
